c500f5151ae2f06d2b1aa94a9e22f4d7ad9179b6
fasdjkherig <farahat.farahat14141679@nursing.menofia.edu.eg>
2023-03-20 20:17:25 +0200

    first commit

--- client/src/scenes/navbar/index.jsx
+++ client/src/scenes/navbar/index.jsx
@@ -69,47 +69,61 @@ const Navbar = () => {
             </IconButton>
           </FlexBetween>
         )}
-
-        {/* computer nav */}
-        {isNonMobileScreens ? (
-          <FlexBetween>
-            <IconButton onClick={() => dispatch(setMode())}>
-              {theme.palette.mode === "dark" ? (
-                <DarkMode sx={{ fontSize: "25px" }} />
-              ) : (
-                <LightMode sx={{ color: dark, fontSize: "25px" }} />
-              )}
-            </IconButton>
-
-            <Message sx={{ fontSize: "25px" }} />
-
-            <Notifications sx={{ fontSize: "25px" }} />
-
-            <Help sx={{ fontSize: "25px" }} />
-
-            <FormControl variant="standard" value={fullName}>
-              <Select
-                value={fullName}
-                sx={{
-                  backgroundColor: neutralLight,
-                  width: "150px",
-                  borderRadius: "0.25rem",
-                  p: "0.25rem 1rem",
-                  "& .MuiSvgIcon-root": {
-                    pr: "0.25rem",
-                    width: "3rem",
-                  },
-                  "& .MuiSelect-select:focus": {
-                    backgroundColor: neutralLight,
-                  },
-                }}
-              ></Select>
-            </FormControl>
-          </FlexBetween>
-        ) : (
-          "dd"
-        )}
       </FlexBetween>
+
+      {/* computer nav */}
+      {isNonMobileScreens ? (
+        <FlexBetween>
+          <IconButton onClick={() => dispatch(setMode())}>
+            {theme.palette.mode === "dark" ? (
+              <DarkMode sx={{ fontSize: "25px" }} />
+            ) : (
+              <LightMode sx={{ color: dark, fontSize: "25px" }} />
+            )}
+          </IconButton>
+
+          <Message sx={{ fontSize: "25px" }} />
+
+          <Notifications sx={{ fontSize: "25px" }} />
+
+          <Help sx={{ fontSize: "25px" }} />
+
+          <FormControl variant="standard" value={fullName}>
+            <Select
+              value={fullName}
+              sx={{
+                backgroundColor: neutralLight,
+                width: "150px",
+                borderRadius: "0.25rem",
+                p: "0.25rem 1rem",
+                "& .MuiSvgIcon-root": {
+                  pr: "0.25rem",
+                  width: "3rem",
+                },
+                "& .MuiSelect-select:focus": {
+                  backgroundColor: neutralLight,
+                },
+              }}
+              input={<InputBase />}
+            >
+              <MenuItem value={fullName}>
+                <Typography>{fullName}</Typography>
+              </MenuItem>
+
+              <MenuItem onClick={() => dispatch(setLogout())}>Log Out</MenuItem>
+            </Select>
+          </FormControl>
+        </FlexBetween>
+      ) : (
+        <IconButton
+          onClick={() => setIsMobileMenuToggled(!isMobileMenuToggled)}
+        >
+          <Menu />
+        </IconButton>
+      )}
+
+      {/* mobile nav */}
+      {!isNonMobileScreens && isMobileMenuToggled && <Box></Box>}
     </FlexBetween>
   );
 };
