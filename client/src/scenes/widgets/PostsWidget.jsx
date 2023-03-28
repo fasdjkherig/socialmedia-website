e747d16773fb2f9c6a194069e4bccb0d2620775c
fasdjkherig <farahat.farahat14141679@nursing.menofia.edu.eg>
2023-03-28 22:07:06 +0200

    5th commit

---
+++ client/src/scenes/widgets/PostsWidget.jsx
@@ -0,0 +1,79 @@
+import React, { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { setPosts } from "../../state";
+import PostWidget from "./PostWidget";
+const PostsWidget = ({ userId, isProfile = false }) => {
+  const dispatch = useDispatch();
+  const posts = useSelector((state) => state.posts);
+  const token = useSelector((state) => state.token);
+
+  //   get all posts
+  const getPosts = async () => {
+    const response = await fetch("http://localhost:3001/posts", {
+      method: "GET",
+      headers: { Authorization: `Bearer ${token}` },
+    });
+
+    // to translate request into json format(parsable format)
+    const data = await response.json();
+
+    dispatch(setPosts({ posts: data }));
+  };
+
+  //   get user posts
+  const getUserPosts = async () => {
+    const response = await fetch(
+      `http://localhost:3001/posts/${userId}/posts`,
+      {
+        method: "GET",
+        headers: { Authorization: `Bearer ${token}` },
+      }
+    );
+
+    // to translate request into json format(parsable format)
+    const data = await response.json();
+
+    dispatch(setPosts({ posts: data }));
+  };
+
+  useEffect(() => {
+    if (isProfile) {
+      getUserPosts();
+    } else {
+      getPosts();
+    }
+  }, []);
+  return (
+    <>
+      {posts.map(
+        ({
+          _id,
+          userId,
+          firstName,
+          lastName,
+          description,
+          location,
+          picturePath,
+          userPicturePath,
+          likes,
+          comments,
+        }) => (
+          <PostWidget
+            key={_id}
+            postId={_id}
+            postUserId={userId}
+            name={`${firstName} ${lastName}`}
+            description={description}
+            location={location}
+            picturePath={picturePath}
+            userPicturePath={userPicturePath}
+            likes={likes}
+            comments={comments}
+          />
+        )
+      )}
+    </>
+  );
+};
+
+export default PostsWidget;
